1ceb9e8f386f113dd11bfd6557c4842a17a49d65
Britannio <britanniojarrett@gmail.com>
2023-11-25 11:40:58 +0000

    left/right input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    private bool jumpKeyWasPressed = false;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (dependent on the frame rate if vsync is off)
     void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            jumpKeyWasPressed = true;
             Debug.Log("Space key was pressed.");
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
         }
 
+        horizontalInput = Input.GetAxis("Horizontal");
 
+
+    }
+
+    // FixedUpdate is called once per physics frame (default is 100 times per second)
+    private void FixedUpdate()
+    {
+
+        if (jumpKeyWasPressed)
+        {
+            jumpKeyWasPressed = false;
+            // Physics updates should be done in FixedUpdate!
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        }
+
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+        
     }
 }
